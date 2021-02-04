cc6f9d40ed60bbc3967c9677670a4a8421c4b95e
yea09 <77084784+yea09@users.noreply.github.com>
2021-02-04 14:59:53 +0900

    Create Marketing.java

---
+++ product/src/main/java/msacoffeechainsample/external/Marketing.java
@@ -0,0 +1,35 @@
+package msacoffeechainsample.external;
+
+public class Marketing {
+
+    private Long id;
+    private Long ordreId;
+    private String process;
+    private Double point;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrdreId() {
+        return ordreId;
+    }
+    public void setOrdreId(Long ordreId) {
+        this.ordreId = ordreId;
+    }
+    public String getProcess() {
+        return process;
+    }
+    public void setProcess(String process) {
+        this.process = process;
+    }
+    public Double getPoint() {
+        return point;
+    }
+    public void setPoint(Double point) {
+        this.point = point;
+    }
+
+}
