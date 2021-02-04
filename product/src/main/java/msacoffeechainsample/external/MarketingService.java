42e44ece54537634cdf0f9091c2df10453d07551
yea09 <77084784+yea09@users.noreply.github.com>
2021-02-04 15:03:24 +0900

    Update MarketingService.java

--- product/src/main/java/msacoffeechainsample/external/MarketingService.java
+++ product/src/main/java/msacoffeechainsample/external/MarketingService.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
+
+/* @FeignClient(name="stock", url="${api.stock.url}") */
+
 @FeignClient(name="marketing", url="http://marketing:8080")
 public interface MarketingService {
 
-    @RequestMapping(method= RequestMethod.GET, path="/marketings")
+    @RequestMapping(method= RequestMethod.GET, path="/marketings/pointCancel")
     public void pointCancel(@RequestBody Marketing marketing);
 
 }
